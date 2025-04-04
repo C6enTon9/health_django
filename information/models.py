2b2ff2bd283b95d0baff87a06f3d6dfbfca38d69
guo-gy <guo.gy@outlook.com>
2025-04-04 23:20:03 +0800

    information model

--- information/models.py
+++ information/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Information(models.Model):
+    username = models.CharField(max_length=100)
+    information = models.TextField()
+
+    def __str__(self):
+        return self.username
