26f270b20e3bfc74b63182c847471d2bc0a4799a
guo-gy <guo.gy@outlook.com>
2025-04-05 16:18:35 +0800

    inforamtion migration

--- information/models.py
+++ information/models.py
@@ -3,6 +3,18 @@ from django.db import models
 class Information(models.Model):
     username = models.CharField(max_length=100)
     information = models.TextField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    age = models.IntegerField()
+    
+    def get_age(self):
+        return self.age
 
+    def get_height(self):
+        return self.height  
+    
+    def get_weight(self):  
+        return self.weight
+    
     def __str__(self):
         return self.username
