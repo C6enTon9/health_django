76dbf16403f8bf8b2552058131e0e70b134eb40c
guo-gy <guo.gy@outlook.com>
2025-04-05 20:36:10 +0800

    edit plan model

--- plan/models.py
+++ plan/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Plan(models.Model):
-    name = models.CharField(max_length=100)
+    user_id = models.CharField(max_length=20)
+    thing = models.CharField(max_length=100)
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
-    
-    def __str__(self):
-        return self.name
