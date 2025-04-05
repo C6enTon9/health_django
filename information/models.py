5274cd6cf2baf9afd21dbf7af0b0ba1913ecbdc9
guo-gy <guo.gy@outlook.com>
2025-04-05 18:54:28 +0800

    information_models

--- information/models.py
+++ information/models.py
@@ -1,20 +1,9 @@
 from django.db import models
 
 class Information(models.Model):
-    username = models.CharField(max_length=100)
+    user_id = models.CharField(max_length=100)
     information = models.TextField()
+    target = models.TextField()
     height = models.IntegerField()
     weight = models.IntegerField()
     age = models.IntegerField()
-    
-    def get_age(self):
-        return self.age
-
-    def get_height(self):
-        return self.height  
-    
-    def get_weight(self):  
-        return self.weight
-    
-    def __str__(self):
-        return self.username
